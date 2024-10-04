80ec3e5e821d7691dab7f74c4145dc746d00ed9b
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2024-10-04 12:54:15 +0700

    add esp32-c3 core board

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
-
+#ifdef BOARD_ESP32_DEVKITM_V1
+  #define LED_PIN 4 // Define the pin for the LED
+#elif BOARD_ESP32_C3_CORE
+  #define LED_PIN 12 // Define the pin for the LED  
+#endif
 void setup() {
-  pinMode(4, OUTPUT); // Set pin 4 to output
+  pinMode(LED_PIN, OUTPUT); // Set pin 4 to output
   Serial.begin(9600); // Initialize serial communication at 9600 bits per second
 }
 
 void loop() {
-  digitalWrite(4, HIGH); // Turn on the LED on pin 4
+  digitalWrite(LED_PIN, HIGH); // Turn on the LED on pin 4
   delay(500); // Wait for 500 milliseconds
-  digitalWrite(4, LOW); // Turn off the LED on pin 4
+  digitalWrite(LED_PIN, LOW); // Turn off the LED on pin 4
   Serial.println("Hello World"); // Print "Hello World" to the serial monitor
   delay(2000); // Wait for 2 seconds (2000 milliseconds)
 }
